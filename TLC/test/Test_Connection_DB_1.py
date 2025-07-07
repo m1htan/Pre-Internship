6c5ae55f71deed398496984ac43232dec68b838b
MayAnhhh163 <huyenhuyen893@gmail.com>
2025-07-07 21:54:44 +0700

    đang sửa bảng fact

--- TLC/test/Test_Connection_DB_1.py
+++ TLC/test/Test_Connection_DB_1.py
@@ -1,8 +1,7 @@
-from TLC.config_sql_server.config_sql_server import config_sql_server_ods
-
-conn_ods = config_sql_server_ods()
+from TLC.config_sql_server.config_sql_server import config_sql_server
+conn_ods = config_sql_server(section='sqlserver_dtm')
 cursor = conn_ods.cursor()
-cursor.execute("SELECT * FROM [ods_date]")
+cursor.execute("SELECT * FROM [f_barchart_HON25_uco_price]")
 
 data = cursor.fetchall()
 print("Sample data:", data[:5])  # In 5 dòng đầu tiên
