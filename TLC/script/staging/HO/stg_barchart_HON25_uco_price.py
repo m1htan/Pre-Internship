53816382a23b296c64e1fe2f1ebf7a17f0ab85c2
Minh Tan <ngminhtan0410@gmail.com>
2025-07-08 17:54:17 +0700

    update staging code
    col prev_open to open

--- TLC/script/staging/HO/stg_barchart_HON25_uco_price.py
+++ TLC/script/staging/HO/stg_barchart_HON25_uco_price.py
@@ -182,9 +182,12 @@ def insert_dataframe_to_temp(df, conn, temp_name):
     table = f"{temp_name}"
     cols = list(df.columns)
 
-    col_list = ','.join(f'[{c}]' for c in cols)
+    col_list = ', '.join(f'[{col}]' for col in cols)
+
     placeholders = ','.join(['?'] * len(cols))
+
     insert_sql = f"INSERT INTO {table} ({col_list}) VALUES ({placeholders})"
+    print("[DEBUG] SQL:", insert_sql)
 
     tuples = [tuple(None if pd.isna(x) else x for x in row) for row in df.to_numpy()]
 
@@ -215,15 +218,15 @@ def insert_new_records(conn, table_name, temp_name):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     insert_cols = (
-        "timing, contract, open, high, low, last, price_change, "
-        "percent_change, volume, oi, raw, source_table, created_date, "
-        "snapshot_date, snapshot_date_oi"
+        "[timing], [contract], [open], [high], [low], [last], [price_change], "
+        "[percent_change], [volume], [oi], [raw], [source_table], [created_date], "
+        "[snapshot_date], [snapshot_date_oi]"
     )
 
     query = f"""
         INSERT INTO {table_name} ({insert_cols})
         SELECT
-            timing, contract, open, high, low, last, price_change,
+            timing, contract, [open], high, low, last, price_change,
             percent_change, volume, oi, raw,
             '{temp_name}', '{now}',
             CAST(timing AS DATE),
